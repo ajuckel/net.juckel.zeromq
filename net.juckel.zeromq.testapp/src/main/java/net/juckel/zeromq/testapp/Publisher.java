11bb1e7b879097cb9d5284a4cd621cffe523a0fe
Anthony W. Juckel <ajuckel@gmail.com>
2012-03-04 22:42:15 -0600

    Update formatting.

--- net.juckel.zeromq.testapp/src/main/java/net/juckel/zeromq/testapp/Publisher.java
+++ net.juckel.zeromq.testapp/src/main/java/net/juckel/zeromq/testapp/Publisher.java
@@ -9,7 +9,8 @@ import org.zeromq.ZMQ;
 
 public class Publisher implements IApplication {
     public Object start(IApplicationContext context) throws Exception {
-        final String[] args = (String[]) context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
+        final String[] args = (String[]) context.getArguments().get(
+                IApplicationContext.APPLICATION_ARGS);
         if (args.length != 2) {
             System.out.println("usage: publisher <bind-to> <msg-count>");
             return Integer.valueOf(-1);
@@ -21,7 +22,7 @@ public class Publisher implements IApplication {
         ZMQ.Socket pub = ctx.socket(ZMQ.PUB);
         pub.bind(bindTo);
 
-        final String[] topics = { "FOO", "BAR", "BAZ" }; 
+        final String[] topics = { "FOO", "BAR", "BAZ" };
         for (int i = 0; i < Integer.parseInt(args[1]); i++) {
             pub.send(topics[i % 3].getBytes(utf8), ZMQ.SNDMORE);
             byte[] message = new byte[8];
--- net.juckel.zeromq.testapp/src/main/java/net/juckel/zeromq/testapp/Subscriber.java
+++ net.juckel.zeromq.testapp/src/main/java/net/juckel/zeromq/testapp/Subscriber.java
@@ -9,35 +9,37 @@ import org.zeromq.ZMQ;
 
 public class Subscriber implements IApplication {
 
-	public Object start(IApplicationContext context) throws Exception {
-		final String[] args = (String[]) context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
-		if (args.length != 2) {
-			System.out.println("usage: subscriber <connect-to> <key>");
-			return Integer.valueOf(-1);
-		}
+    public Object start(IApplicationContext context) throws Exception {
+        final String[] args = (String[]) context.getArguments().get(
+                IApplicationContext.APPLICATION_ARGS);
+        if (args.length != 2) {
+            System.out.println("usage: subscriber <connect-to> <key>");
+            return Integer.valueOf(-1);
+        }
 
         final Charset utf8 = Charset.forName("UTF8");
-		String connectTo = args[0];
-		ZMQ.Context ctx = ZMQ.context(1);
-		ZMQ.Socket sub = ctx.socket(ZMQ.SUB);
-		sub.connect(connectTo);
+        String connectTo = args[0];
+        ZMQ.Context ctx = ZMQ.context(1);
+        ZMQ.Socket sub = ctx.socket(ZMQ.SUB);
+        sub.connect(connectTo);
 
-		String filter = args[1];
-		sub.subscribe(filter.getBytes(utf8));
+        String filter = args[1];
+        sub.subscribe(filter.getBytes(utf8));
 
-		double total = 0.0d;
-		while (true) {
-			String msg = new String(sub.recv(0), utf8).trim();
-			byte[] payload = sub.recv(0);
-			total += ByteBuffer.wrap(payload).getDouble();
-            System.out.println("Message received: " + msg + "; Running total: " + total);
+        double total = 0.0d;
+        while (true) {
+            String msg = new String(sub.recv(0), utf8).trim();
+            byte[] payload = sub.recv(0);
+            total += ByteBuffer.wrap(payload).getDouble();
+            System.out.println("Message received: " + msg + "; Running total: "
+                    + total);
 
-		}
-	}
+        }
+    }
 
-	public void stop() {
-		// TODO Auto-generated method stub
+    public void stop() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }
